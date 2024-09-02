9ad4a4b62d709ae81e6eecd5843a12d14f411741
Samuele Salvatico <samuele.salvatico@nearform.com>
2024-09-02 16:26:12 +0200

    chore(phrases): updated errors

--- packages/phrases/src/locales/pl-pl/errors/organization.ts
+++ packages/phrases/src/locales/pl-pl/errors/organization.ts
@@ -1,5 +1,11 @@
 const organization = {
   require_membership: 'Użytkownik musi być członkiem organizacji, aby kontynuować.',
+  // OGCIO
+  default_organization_missing:
+    'Niektóre domyślne organizacje nie istnieją w bazie danych, upewnij się, że najpierw utworzono organizacje',
+  // OGCIO
+  default_organization_role_missing:
+    'Niektóre z domyślnych ról organizacji nie istnieją w bazie danych, upewnij się, że najpierw utworzono role organizacji',
 };
 
 export default Object.freeze(organization);
