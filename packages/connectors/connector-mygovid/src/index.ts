8ed43660436a594b33d33a43aa8f801b7ae78b88
Samuele Salvatico <samuele.salvatico@nearform.com>
2025-01-03 10:12:35 +0100

    Revert "chore(connectors): added logging when validatirng jwt (#174)"
    This reverts commit c01314603e382d9b827019707ec83ee2e2be6ab4.

--- packages/connectors/connector-mygovid/src/index.ts
+++ packages/connectors/connector-mygovid/src/index.ts
@@ -17,7 +17,6 @@ import {
   jsonGuard,
 } from '@logto/connector-kit';
 import { constructAuthorizationUri } from '@logto/connector-oauth';
-import { ConsoleLog } from '@logto/shared';
 import { generateStandardId } from '@logto/shared/universal';
 import { createRemoteJWKSet, jwtVerify } from 'jose';
 import { HTTPError } from 'ky';
@@ -25,8 +24,6 @@ import { HTTPError } from 'ky';
 import { defaultMetadata } from './constant.js';
 import { myGovIdTokenProfileStandardClaimsGuard, oidcConnectorConfigGuard } from './types.js';
 import { getIdToken } from './utils.js';
-
-export const consoleLog: ConsoleLog = new ConsoleLog();
 
 const generateNonce = () => generateStandardId();
 
@@ -99,18 +96,6 @@ const getUserInfo =
     }
 
     try {
-      // OGCIO
-      consoleLog.warn('Logging jwt verify params', {
-        idToken,
-        createRemoteJWKSet: createRemoteJWKSet(
-          new URL(parsedConfig.idTokenVerificationConfig.jwksUri)
-        ),
-        thirdParam: {
-          ...parsedConfig.idTokenVerificationConfig,
-          audience: parsedConfig.clientId,
-        },
-      });
-
       const { payload } = await jwtVerify(
         idToken,
         createRemoteJWKSet(new URL(parsedConfig.idTokenVerificationConfig.jwksUri)),
