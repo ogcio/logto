f40a90b32a5ccd7b248556e41cc6b9939fe3eea2
Samuele Salvatico <samuele.salvatico@nearform.com>
2025-01-29 09:26:57 +0100

    removed useless comment

--- packages/cli/src/commands/database/ogcio/users.ts
+++ packages/cli/src/commands/database/ogcio/users.ts
@@ -211,56 +211,6 @@ const buildWebhookPayload = (user: UserSeeder) => {
   };
 };
 
-/* 
-{
-    hookId: Type.Optional(NullableString),
-    event: Type.Optional(NullableString),
-    sessionId: Type.Optional(NullableString),
-    userAgent: Type.Optional(NullableString),
-    ip: Type.Optional(NullableString),
-    path: Type.Optional(NullableString),
-    method: Type.Optional(NullableString),
-    status: Type.Optional(Type.Number()),
-    createdAt: Type.Optional(Type.String({ format: "date-time" })),
-    data: Type.Object({
-      id: Type.String(),
-      username: NullableString,
-      primaryEmail: Type.String({ format: "email" }),
-      primaryPhone: Type.Optional(NullableString),
-      name: Type.Optional(NullableString),
-      avatar: Type.Optional(NullableString),
-      customData: Type.Object({
-        profileImportId: Type.Optional(NullableString),
-        organizationId: Type.Optional(NullableString),
-      }),
-      identities: Type.Record(
-        Type.String(),
-        Type.Object({
-          details: Type.Object({
-            email: Type.Optional(NullableString),
-            rawData: Type.Record(
-              Type.String(),
-              Type.Union([
-                Type.String(),
-                Type.Null(),
-                Type.Number(),
-                Type.Boolean(),
-              ]),
-            ),
-          }),
-        }),
-      ),
-      lastSignInAt: Type.Optional(Type.Union([Type.Number(), Type.Null()])),
-      createdAt: Type.Optional(Type.Number()),
-      updatedAt: Type.Optional(Type.Number()),
-      profile: Type.Optional(Type.Record(Type.String(), Type.Unknown())),
-      applicationId: Type.Optional(NullableString),
-      isSuspended: Type.Optional(Type.Boolean()),
-      hasPassword: Type.Optional(Type.Boolean()),
-    }),
-  }
-*/
-
 const sign = (signingKey: string, payload: Record<string, unknown>) => {
   const hmac = createHmac('sha256', signingKey);
   const payloadString = JSON.stringify(payload);
