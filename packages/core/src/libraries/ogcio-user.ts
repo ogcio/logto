6362f9bf5d0bf7d9591ac650dd64f9c967de5e36
Norbert Nagy <nagy.norbert1989@gmail.com>
2025-04-17 17:46:52 +0300

    feat: assign default user role (#244)
    * feat: assign default user role
    
    * Update packages/core/src/libraries/ogcio-constants.ts
    
    Co-authored-by: Andrea Cappadona <19609031+andreacappadona17@users.noreply.github.com>
    
    * feat: update
    
    ---------
    
    Co-authored-by: Andrea Cappadona <19609031+andreacappadona17@users.noreply.github.com>

--- packages/core/src/libraries/ogcio-user.ts
+++ packages/core/src/libraries/ogcio-user.ts
@@ -5,143 +5,20 @@ import {
   type Role,
   type User,
   type Organization,
-  type OrganizationRole,
   adminTenantId,
 } from '@logto/schemas';
 import { generateStandardId } from '@logto/shared';
-import { deduplicate } from '@silverhand/essentials';
 import { type QueryResult, type QueryResultRow } from '@silverhand/slonik';
 
-import { EnvSet } from '#src/env-set/index.js';
 import type OrganizationQueries from '#src/queries/organization/index.js';
-import assertThat from '#src/utils/assert-that.js';
 
-import { OGCIO_ORGANIZATION_ROLES, OGCIO_ORGANIZATIONS, OGCIO_ROLES } from './ogcio-constants.js';
-
-const getDefaultOrganizationsForUser = async (organizationQueries: OrganizationQueries) => {
-  const organizationNames: string[] = deduplicate(EnvSet.values.userDefaultOrganizationNames);
-  consoleLog.info('DEFAULT ORG NAMES', organizationNames);
-  if (organizationNames.length === 0) {
-    return [];
-  }
-  const lowerOrganizationNames = new Set(organizationNames.map((name) => name.toLowerCase()));
-  const allOrganizations = await organizationQueries.findAll();
-
-  const outputOrgs = allOrganizations[1].filter((fromDatabaseOrg: Organization) =>
-    lowerOrganizationNames.has(fromDatabaseOrg.name.toLowerCase())
-  );
-
-  assertThat(
-    outputOrgs.length === organizationNames.length,
-    'organization.default_organization_missing'
-  );
-
-  return outputOrgs;
-};
-
-const getDefaultOrganizationRolesForUser = async (organizationQueries: OrganizationQueries) => {
-  const roleNames: string[] = deduplicate(EnvSet.values.userDefaultOrganizationRoleNames);
-  consoleLog.info('DEFUALT ORG ROLE NAMES', roleNames);
-  if (roleNames.length === 0) {
-    return [];
-  }
-  const lowerRoleNames = new Set(roleNames.map((name) => name.toLowerCase()));
-  const limit = 200;
-  const offset = 0;
-  // eslint-disable-next-line @silverhand/fp/no-let
-  let outputRoleNames: OrganizationRole[] = [];
-  // eslint-disable-next-line @silverhand/fp/no-let
-  let foundCount = 1;
-  while (outputRoleNames.length < lowerRoleNames.size && foundCount > 0) {
-    // eslint-disable-next-line no-await-in-loop
-    const allOrganizations = await organizationQueries.roles.findAll(limit, offset);
-    // eslint-disable-next-line @silverhand/fp/no-mutation
-    foundCount = allOrganizations[0];
-    // eslint-disable-next-line @silverhand/fp/no-mutation
-    outputRoleNames = [
-      ...outputRoleNames,
-      ...allOrganizations[1].filter((fromDatabaseOrg: OrganizationRole) =>
-        lowerRoleNames.has(fromDatabaseOrg.name.toLowerCase())
-      ),
-    ];
-  }
-
-  assertThat(
-    outputRoleNames.length === lowerRoleNames.size,
-    'organization.default_organization_role_missing'
-  );
-
-  return outputRoleNames;
-};
-
-const getOrganizationRelationsForUser = async (organizationQueries: OrganizationQueries) => {
-  return {
-    organizations: await getDefaultOrganizationsForUser(organizationQueries),
-    roles: await getDefaultOrganizationRolesForUser(organizationQueries),
-  };
-};
-
-const insertOrganizationRelationsForUser = async (params: {
-  userId: string;
-  organizationQueries: OrganizationQueries;
-  organizations: Organization[];
-  roles: OrganizationRole[];
-}) => {
-  if (params.organizations.length === 0) {
-    return;
-  }
-
-  await Promise.all(
-    params.organizations.map(async (org) =>
-      params.organizationQueries.relations.users.insert({
-        organizationId: org.id,
-        userId: params.userId,
-      })
-    )
-  );
-
-  if (params.roles.length > 0) {
-    const rolesMappings: Array<{
-      organizationId: string;
-      organizationRoleId: string;
-      userId: string;
-    }> = [];
-    for (const role of params.roles) {
-      for (const org of params.organizations) {
-        // eslint-disable-next-line @silverhand/fp/no-mutating-methods
-        rolesMappings.push({
-          organizationId: org.id,
-          organizationRoleId: role.id,
-          userId: params.userId,
-        });
-      }
-    }
-
-    await Promise.all(
-      rolesMappings.map(async (roleMap) =>
-        params.organizationQueries.relations.usersRoles.insert(roleMap)
-      )
-    );
-  }
-};
-
-export const manageDefaultOrganizations = async (params: {
-  userId: string;
-  organizationQueries: OrganizationQueries;
-}) => {
-  const orgRelations = await getOrganizationRelationsForUser(params.organizationQueries);
-  await insertOrganizationRelationsForUser({
-    userId: params.userId,
-    ...orgRelations,
-    organizationQueries: params.organizationQueries,
-  });
-};
-
-const PUBLIC_SERVANT_DOMAINS = new Set(['gov.ie']);
-
-const getDomainFromEmail = (email: string): string | undefined => {
-  return email.split('@')[1];
-};
+import {
+  OGCIO_ENTRA_ID_IDENTITY,
+  OGCIO_MY_GOV_ID_IDENTITY,
+  OGCIO_ORGANIZATION_ROLES,
+  OGCIO_ORGANIZATIONS,
+  OGCIO_ROLES,
+} from './ogcio-constants.js';
 
 const assignCitizenRole = async (
   user: User,
@@ -204,6 +81,10 @@ const assignInactivePublicServantRole = async (
   await assignOrganizationRoleToUser(user, organization, organizationQueries);
 };
 
+const getUserIdentities = (user: User) => {
+  return Object.keys(user.identities);
+};
+
 export const manageDefaultUserRole = async (
   user: User,
   getRoles: (id: string) => Promise<Role>,
@@ -216,21 +97,12 @@ export const manageDefaultUserRole = async (
     return;
   }
 
-  if (user.primaryEmail === null) {
-    consoleLog.error(phrases.en.errors.user.email_not_exist);
-    return;
-  }
+  const identities = getUserIdentities(user);
 
-  const domain = getDomainFromEmail(user.primaryEmail);
-
-  if (domain === undefined) {
-    consoleLog.error(phrases.en.errors.user.invalid_email);
-    return;
-  }
-
-  if (PUBLIC_SERVANT_DOMAINS.has(domain)) {
+  if (identities.includes(OGCIO_ENTRA_ID_IDENTITY)) {
     return assignInactivePublicServantRole(user, organizationQueries);
   }
-
-  return assignCitizenRole(user, getRoles, insertUsersRoles);
+  if (identities.includes(OGCIO_MY_GOV_ID_IDENTITY)) {
+    return assignCitizenRole(user, getRoles, insertUsersRoles);
+  }
 };
