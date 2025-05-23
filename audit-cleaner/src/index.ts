b7e76039c740f83b189e5895b4f57715fd7c4329
Norbert Nagy <norbert.nagy@nearform.com>
2025-05-23 09:20:12 +0300

    feat: set log level

--- audit-cleaner/src/index.ts
+++ audit-cleaner/src/index.ts
@@ -11,6 +11,7 @@ import prettyMilliseconds from "pretty-ms";
 import { pino } from 'pino';
 
 const logger = pino({
+  level: process.env.LOG_LEVEL || 'info',
   transport: {
     target: 'pino-pretty',
     options: {
