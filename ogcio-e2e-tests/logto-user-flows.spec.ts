1f5b2b257c6821a4500c6b7ba8144d9b1a275507
Mike Clarkson <michael.clarkson@nearform.com>
2025-07-07 12:40:30 +0100

    feat: adding deletion of roles and user

--- ogcio-e2e-tests/logto-user-flows.spec.ts
+++ ogcio-e2e-tests/logto-user-flows.spec.ts
@@ -15,7 +15,7 @@ if (!TEST_PASSWORD) {
 
 test.describe('Logto User Flows - OGCIO E2E Tests', () => {
 
-    test('Possible roles for new users should be correct', async ({ page }) => {
+    test('An admin can create user and assign/delete roles and then delete the user', async ({ page }) => {
         const username = 'playwrightusername';
 
         await page.goto(LOGTO_ADMIN_URL);
@@ -23,20 +23,41 @@ test.describe('Logto User Flows - OGCIO E2E Tests', () => {
         await page.locator('input[name="password"]').fill(TEST_PASSWORD);
         await page.getByRole('button', { name: 'Sign in' }).click();
 
+        //Add new user
         await page.getByRole('link', { name: 'User management' }).click();
         await page.getByRole('button', { name: 'Add user' }).click();
-        //input user details, email, phonenumber, username and name
         await page.getByRole('textbox').nth(0).fill('playwrighttest@test.com');
         await page.getByRole('textbox').nth(1).fill('123456789');
         await page.getByRole('textbox').nth(2).fill(username);
         await page.getByRole('textbox').nth(3).fill('playwright user');
-        //click add user button
         await page.getByRole('button', { name: 'Add user' }).click();
+
+        //Navigate to user details > Roles
         await page.getByRole('button', { name: 'Check user detail' }).click();
         await page.getByRole('navigation').getByRole('link', { name: 'Roles' }).click();
         await page.getByRole('button', { name: 'Assign roles' }).click();
         await expect(page.getByRole('button', { name: 'Onboarded citizen' })).toBeVisible();
-        await expect(page.getByRole('button', { name: 'Citizen' })).toBeVisible();
+        await expect(page.getByRole('button', { name: 'Citizen', exact: true })).toBeVisible();
         await expect(page.getByRole('button', { name: 'FormsIE Admin' })).toBeVisible();
+
+        //check all 3 roles can be assigned
+        await page.getByRole('button', { name: 'Citizen', exact: true }).click();
+        await page.getByRole('button', { name: 'FormsIE Admin' }).click();
+        await page.getByRole('button', { name: 'Onboarded citizen' }).click();
+        await page.getByRole('button', { name: 'Assign role' }).click();
+
+        //check all 3 roles can be deleted
+        await page.getByRole('row', { name: 'Citizen' }).first().getByRole('button').click();
+        await page.getByRole('button', { name: 'Remove' }).click();
+        await page.getByRole('row', { name: 'FormsIE Admin' }).getByRole('button').click();
+        await page.getByRole('button', { name: 'Remove' }).click();
+        await page.getByRole('row', { name: 'Onboarded citizen' }).getByRole('button').click();
+        await page.getByRole('button', { name: 'Remove' }).click();
+
+        //delete the user
+        await page.locator('button').nth(1).click();
+        await page.getByRole('menuitem', { name: 'Delete' }).click();
+        await page.getByRole('button', { name: 'Delete' }).click();
+        await expect(page.getByRole('row', { name: username })).not.toBeVisible();
     });
 });
