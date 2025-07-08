f342ec8efacb1d3db6de9ae83087b60a1c76351a
Mike Clarkson <michael.clarkson@nearform.com>
2025-07-08 14:02:25 +0100

    feat: adding video

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,8 @@ export default defineConfig({
 		/* Base URL to use in actions like `await page.goto('/')`. */
 		baseURL: process.env.LOGTO_ADMIN_URL || 'http://localhost:3302',
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-		trace: 'on-first-retry',
+		trace: 'retain-on-failure',
+		video: 'retain-on-failure',
 		/* Increase timeouts for CI environment */
 		navigationTimeout: process.env.CI ? 60000 : 30000,
 		actionTimeout: process.env.CI ? 30000 : 10000,
